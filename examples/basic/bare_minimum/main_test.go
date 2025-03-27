843eb11f4554a2041b4d83b18bd3cf4e47795101
JT Archie <jtarchie@gmail.com>
2025-03-27 16:49:44 -0600

    fix assertion message

--- examples/basic/bare_minimum/main_test.go
+++ examples/basic/bare_minimum/main_test.go
@@ -18,7 +18,7 @@ func TestBareMinimum(t *testing.T) {
 	a.Expect(response.Agent.Name()).To(gomega.Equal("HelpfulAgent"))
 
 	result, err := assert.Agent(
-		fmt.Sprintf("This messages should be a positive greeting to a user: %q", response.Messages[1].Content),
+		fmt.Sprintf("This should be a greeting: %q", response.Messages[1].Content),
 	)
 	a.Expect(err).NotTo(gomega.HaveOccurred())
 	a.Expect(result.Status).To(gomega.Equal(assert.Success), result.Explanation)
